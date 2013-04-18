8218d768118967ee29a9a2de732249b0ee37d267
Marco Valtas <mavcunha@gmail.com>
2013-04-18 08:12:17 -0300

    Reordering setup for clarification

--- test/com/gsaex/AlignmentTest.java
+++ test/com/gsaex/AlignmentTest.java
@@ -18,9 +18,9 @@ public class AlignmentTest {
         expectedMatrix.set(new Element(new Coord(0,1), -10));
         expectedMatrix.set(new Element(new Coord(0,2), -20));
         expectedMatrix.set(new Element(new Coord(1,0), -10));
-        expectedMatrix.set(new Element(new Coord(2,0), -20));
         expectedMatrix.set(new Element(new Coord(1,1), 10));
         expectedMatrix.set(new Element(new Coord(1,2), 0));
+        expectedMatrix.set(new Element(new Coord(2,0), -20));
         expectedMatrix.set(new Element(new Coord(2,1), 0));
         expectedMatrix.set(new Element(new Coord(2,2), 20));
 
